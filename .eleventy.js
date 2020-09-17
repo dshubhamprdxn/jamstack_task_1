2ac19ff908e75e237d43b3c4f5fbb33928d5f396
Shubham <99sdawkhar@gmail.com>
2020-09-17 14:37:05 +0530

    taxonomy tags added

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
-module.export = config => {
-    eleventyConfig.addFilter("animal", require("./_includes/postlist.njk"));
-    config.addFilter('animal', arr => {
-        const fav = post => post.data.animal;
-        return arr.filter(fav);
-    })
-}
+// module.export = config => {
+//     eleventyConfig.addFilter("animal", require("./_includes/postlist.njk"));
+//     config.addFilter('animal', arr => {
+//         const fav = post => post.data.animal;
+//         return arr.filter(fav);
+//     })
+// }
